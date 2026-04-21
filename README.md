af716c2c6fe87c1eff69285fdf7d8f7d64fa7723
noobOriginals <noobOriginals@xx.com>
2026-04-22 01:08:28 +0300

    Updated main.cpp notice, and added the README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pathtraced
+
+A simple CPU pathtracer made from scratch, with the goal of enforcing basic raytracing/pathtracing notions.
+
+<br>
+
+---
+
+### NOTICE (to anyone from school who may stumble onto this project):
+
+Being a large project and containing many files, it's rather hard to import/export to/from Code::Blocks. So I kindly ask of you to take care of this project, in the event that you need to delete it. You could upload it to drive or anything like that, but please consider not rendering all of this work pointless.
+
+<br>
+
+---
+
+**Project repository**: https://github.com/noobOriginals/Pathtraced
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 /**
-    To whom may stumble onto this project:
-    The goal of this project is to create a CPU pathtracer from scratch.
-    Being a large project and containing many files, it's rather hard to import/export.
-    So I kindly ask of you to take care of this project, in the event that you need to delete it.
-    You could upload it to drive or anything like that, but please consider not rendering all of this work pointless.
+    As this is also a project that's being worked on from school, the following message is meant for
+    anyone who may stumble onto this project (on the coputers at school):
+
+    The goal of this project is to create a CPU pathtracer from scratch. Being a large project and
+    containing many files, it's rather hard to import/export to/from Code::Blocks. So I kindly ask of you to take care of
+    this project, in the event that you need to delete it. You could upload it to drive or anything
+    like that, but please consider not rendering all of this work pointless.
+
+    Repository of this project: https://github.com/noobOriginals/Pathtraced
  */
 
 // Std includes
